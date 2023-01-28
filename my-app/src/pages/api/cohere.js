147c81f9aeeb020b9579a7e919029dcdae4c4104
sinancang <sinancangulan@gmail.com>
2023-01-28 16:54:16 -0500

    cohere API request

--- my-app/src/pages/api/cohere.js
+++ my-app/src/pages/api/cohere.js
@@ -1,11 +1,26 @@
 const cohere = require('cohere-ai');
 
+cohere.init('FEOSf1ZmJfkWKBcgsE5eClAShxABsSC2TEbzn4o2')
+
 export default async function handler(req, res) {
-    cohere.init('FEOSf1ZmJfkWKBcgsE5eClAShxABsSC2TEbzn4o2')
+    const name = req.body['name'];
+    const feeling = req.body['feeling'];
+    const action = "staying awake";
+    const pronouns = req.body['pronouns'];
+
+    const prompt = "Write me chaotic evil story where our hero " + name + ", finds herself feeling " +
+        feeling + " today. So she decides to" + action +". What does she see?";
     const response = await cohere.generate({
-            prompt: "what is your name?",
-        }
-    );
-    console.log(response);
-    res.status(200).json({ text: response})
+            model: 'command-xlarge-20221108',
+            prompt: prompt,
+            max_tokens: 300,
+            temperature: 0.9,
+            k: 0,
+            p: 0.75,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            stop_sequences: [],
+            return_likelihoods: 'NONE'
+    });
+    res.status(200).json({ text: `${response.body.generations[0].text}`});
 }
