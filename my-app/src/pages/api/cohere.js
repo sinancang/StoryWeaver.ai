3543299b5f5cae0c1c6d46323edc8b31beffacde
sinancang <sinancangulan@gmail.com>
2023-01-28 21:28:01 -0500

    send form data

--- my-app/src/pages/api/cohere.js
+++ my-app/src/pages/api/cohere.js
@@ -8,8 +8,6 @@ export default async function handler(req, res) {
     const action = req.body['action'];
     const pronouns = req.body['pronouns'];
 
-    console.log(req.body)
-
     const pronounMap = new Map();
     pronounMap.set('he/him', ['he', 'him', 'his', 'himself'])
     pronounMap.set('she/her', ['she', 'her', 'hers', 'herself'])
@@ -18,17 +16,17 @@ export default async function handler(req, res) {
 
     const prompt = "Write me an introduction to a chaotic evil story where our hero " +
         name + ", finds " + pronounMap.get(pronouns)[3] + " feeling " + feeling + " today. So "
-        + pronounMap.get(pronouns)[0] + " decides to " + action +". What does she see?";
+        + pronounMap.get(pronouns)[0] + " decides to " + action +". What does " + pronounMap.get(pronouns)[0] + " see?";
 
     const response = await cohere.generate({
             model: 'command-xlarge-20221108',
             prompt: prompt,
             max_tokens: 300,
-            temperature: 0.9,
+            temperature: 2.0,
             k: 0,
             p: 0.75,
-            frequency_penalty: 0,
-            presence_penalty: 0,
+            frequency_penalty: 0.39,
+            presence_penalty: 0.39,
             stop_sequences: [],
             return_likelihoods: 'NONE'
     });
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -3,7 +3,9 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import React, { useState } from 'react';
+import { MutatingDots } from 'react-loader-spinner'
 import axios from 'axios';
+
 
 // import p5 from 'p5';
 // import '../public/p5js/sketch.js';
@@ -15,20 +17,35 @@ export default function Home() {
     const submitButton = "px-4";
     const buttonField ="bg-white w-4/12 h-5";
 
+    const defaultFormInfo = {
+        'name': 'sinan',
+        'feeling': 'moody',
+        'pronouns': 'he/him',
+        'action': 'dance',
+    }
+
     const [requireInput, setRequireInput] = useState(true);
-    const [prompt, setPrompt] = useState("");
+    const [prompt, setPrompt] = useState(null);
+    const [formInfo, setFormInfo] = useState(defaultFormInfo);
 
     const fetchPrompt = async () => {
-        const data = {
-            'name': 'sinan',
-            'feeling': 'moody',
-            'pronouns': 'he/him',
-            'action': 'dance',
-        }
         setRequireInput(false);
-        const response_API = await axios.post('/api/cohere', data);
-        setPrompt(JSON.stringify(response_API.data.text));
+        const response_API = await axios.post('/api/cohere', formInfo);
+        setPrompt(JSON.stringify(response_API.data.text).replace(/['"]+/g, ''));
     }
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setFormInfo({
+            ...formInfo,
+            [name]: value,
+        });
+    }
+    const handleSubmit = () => {
+        console.log(formInfo);
+        fetchPrompt()
+    }
+
     return (
         <>
           <Head>
@@ -53,7 +70,7 @@ export default function Home() {
           <main className={styles.main}>
             <div className={styles.center}>
               <div className={styles.circles}>
-                <Image
+                <Image className="rounded-md"
                   src="/circles.png"
                   alt="13"
                   width={534}
@@ -62,19 +79,18 @@ export default function Home() {
                 />
               </div>
             </div>
-            <div className="bg-[#005CE7] py-2 px-4 rounded-md text-black focus:outline-none focus:shadow-outline-blue colorbg-midnight leading-8">
-              {/* <!-- Basic HTML Form --> */}
+            <div className="bg-[#005CE7] py-2 px-4 rounded-md text-black focus:outline-none focus:shadow-outline-blue colorbg-midnight leading-8 h-100 w-100">
                 {
                     requireInput ? (
-                    <form action="/api/cohere" method="post">
+                    <form method="post">
                         <label className={submitButton}>What is your name?</label>
-                        <input className={buttonField} type="text" id="name" name="name"/><br/>
+                        <input className={buttonField} onChange={handleInputChange} type="text" id="name" name="name"/><br/>
 
                         <label className={submitButton}>How do you feel today</label>
-                        <input className={buttonField} type="text" id="feeling" name="feeling"/><br/>
+                        <input className={buttonField} onChange={handleInputChange} type="text" id="feeling" name="feeling"/><br/>
 
                         <label className={submitButton}>What are your pronouns?</label>
-                        <select name="pronouns" defaultValue="she/her">
+                        <select name="pronouns" onChange={handleInputChange} defaultValue="she/her">
                             <option name="he/him">he/him</option>
                             <option name="she/her" selected>she/her</option>
                             <option name="they/them">they/them</option>
@@ -82,11 +98,11 @@ export default function Home() {
                         </select><br/>
 
                         <label className={submitButton}>What do you want your character to do?</label>
-                        <input className={buttonField} type="text" id="describe" name="describe"/><br/><br/>
-                        <button className="button-31" role="button" onClick={fetchPrompt}>Submit</button>
+                        <input className={buttonField} type="text" onChange={handleInputChange} id="describe" name="action"/><br/><br/>
+                        <button className="button-31 animate-bounce" role="button" onClick={handleSubmit}>Submit</button>
                     </form>
                     ) : (
-                        <p>{prompt}</p>
+                            prompt ? (<p>{prompt.replace(/\\n/g," ")}</p>) : (<MutatingDots color="#EB4A75" />)
                     )
                 }
             </div>
